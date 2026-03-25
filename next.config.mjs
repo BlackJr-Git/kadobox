5b91339ea631b0b7c45edcbe77521e8607c64c4e
Junior Asosa <juniorassosa@gmail.com>
2026-03-25 12:34:46 +0100

    add image2url as valid images source

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image2url.com",
+        pathname: "/r2/default/images/**",
+      },
+    ],
+  },
+}
 
 export default nextConfig
