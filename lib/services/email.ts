96b06ac99415c76e05118e3598c2f266e6cb4eb4
Junior Asosa <juniorassosa@gmail.com>
2026-03-25 10:59:51 +0100

    change default emails

--- lib/services/email.ts
+++ lib/services/email.ts
@@ -1,5 +1,5 @@
 const RESEND_API_KEY = process.env.RESEND_API_KEY || ""
-const FROM_EMAIL = process.env.FROM_EMAIL || "KDOBOX <noreply@kdobox.com>"
+const FROM_EMAIL = process.env.FROM_EMAIL || "KDOBOX <noreply@cadeauchrono.com>"
 
 type EmailOptions = {
   to: string
