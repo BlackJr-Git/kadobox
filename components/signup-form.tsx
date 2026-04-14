f6de839c6613083ffd40da8293a2efe938b9fd56
Junior Asosa <juniorassosa@gmail.com>
2026-04-14 10:00:01 +0100

    add login image

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -191,7 +191,7 @@ export function SignupForm({
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="/placeholder.svg"
+              src="/kdo_login.jpg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
